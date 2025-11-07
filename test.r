2898f87ec7b4327a6d38bd3614389ae8807fa622
13304057279@163.com <13304057279@163.com>
2025-11-07 16:03:05 +0800

    first commit

---
+++ test.r
@@ -0,0 +1,11 @@
+# 数据准备
+info = c(1, 2, 4, 8)
+
+# 命名
+names = c("Google", "Runoob", "Taobao", "Weibo")
+
+# 涂色（可选）
+cols = c("#ED1C24","#22B14C","#FFC90E","#3f48CC")
+
+# 绘图
+pie(info, labels=names, col=cols)
